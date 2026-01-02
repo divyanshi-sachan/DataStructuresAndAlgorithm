04945d299f5ee7026c10f824494bb609f0427c5b
Divyanshi Sachan <149485853+divyanshi-sachan@users.noreply.github.com>
2026-01-02 09:21:05 +0530

    Time: 0 ms (100.00%) | Memory: 18.7 MB (41.46%) - LeetSync

--- 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.py
+++ 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.py
@@ -3,12 +3,10 @@ class Solution:
         length = len(nums)
         n=length//2
         dict1={}
-        for i in range(len(nums)):
-            if nums[i] not in dict1:
-                dict1[nums[i]] = 1
+        for num in nums:
+            if num not in dict1:
+                dict1[num] = 1
             else:
-                dict1[nums[i]]+=1
-        for i,value in dict1.items():
-            if value == n:
-                return i
+                dict1[num]+=1
+                return num
         return -1
