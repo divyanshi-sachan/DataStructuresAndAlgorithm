7d2e0e34d6c295a315155b4b39c83cf271fe4e19
Divyanshi Sachan <sachandivyanshi376@gmail.com>
2026-08-11 14:20:48 +0530

    Time: 0 ms (100.00%) | Memory: 20 MB (43.14%) - LeetSync

--- 35-search-insert-position/search-insert-position.py
+++ 35-search-insert-position/search-insert-position.py
@@ -5,9 +5,9 @@ class Solution:
         right = n-1
         while left<=right:
             mid = (left+right)//2
-            if nums[mid]>=target:
-                right = mid-1
+            if nums[mid]<target:
+                left = mid+1   
             else:
-                left = mid+1
+                right = mid-1
         return left
         
